0f2dcc10cd9848dc604729da6caa3e36893379cd
Fedorov113 <fedorov.de@gmail.com>
2018-10-26 12:59:04 +0300

    Init commit

---
+++ msys/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+admin.site.register(DatasetHard)
+admin.site.register(DatasetSoft)
+
+admin.site.register(SampleSource)
+admin.site.register(RealSample)
+
+admin.site.register(Library)
+admin.site.register(LibrarySample)
+
+admin.site.register(SequencingRun)
+admin.site.register(MgSample)
+admin.site.register(MgSampleFileContainer)
+admin.site.register(MgFile)
+
