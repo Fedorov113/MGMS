39bd6ad713205664cc73807e63ad3c377bdffcf6
Fedorov113 <fedorov.de@gmail.com>
2018-11-19 23:54:38 +0300

    updates

--- msys/admin.py
+++ msys/admin.py
@@ -2,13 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models.result_models import GeneralResult, ProfileResult
 from .models.entry_models import Schema
-
-
-
-admin.site.register(GeneralResult)
-admin.site.register(ProfileResult)
 
 admin.site.register(Schema)
 
--- msys/api/urls.py
+++ msys/api/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
-
-from msys.result.views import ResultRequest
-from msys.result.views import ResultView
-from msys.api.event_views import *
 from msys.api.entry_views import *
 
 
 urlpatterns = [
-
-    path('result/', ResultView.as_view()),
-    path('result_request/', ResultRequest.as_view()),
-
-
     path('schema_expanded/', SchemaView.as_view()),
 
     path('schema/', SchemaList.as_view()),
